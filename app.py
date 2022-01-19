292f0504bde4dcb785140ee5804b5038e1d9a57e
AJSmith1997 <96802117+AJSmith1997@users.noreply.github.com>
2022-01-18 18:51:22 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import random;
+import math; 
+
+
+
+
+
+
+def walletFunction(num):
+  wallet = (num)
+  user_input = input("hello! how much money would you like to ad to the game? ($10, $20, or $50"),
+  if user_input == '10':
+    return sum(10 + num)
+  if user_input == '20':
+    return sum(20 + num)
+  if user_input == '30':
+    return sum(30 + num)
+  return input("ready for game one?")
+
+
+values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ]
+suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+
+cards = random.choice(values) + random.choice(suits)
+
+handOne = {
+  print(cards)
+}
+
+handTwo = {
+  print(cards)
+}
+
+
+
+
+
+
